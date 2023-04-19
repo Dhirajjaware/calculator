6e7f1430f9ef6f5b9b3413177400dab201f6f685
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-04-19 12:52:10 +0530

    Add calculation history feature

--- script.js
+++ script.js
@@ -1,43 +1,109 @@
-"use strict";
+'use strict';
 
-const containerBtns = document.querySelector(".container__buttons");
+const containerBtns = document.querySelector('.container__buttons');
+const input = document.querySelector('.input__numbers');
+const equal = document.querySelector('.equal');
+const btnBackSpace = document.querySelector('.btn__backspace');
+const canvasContainer = document.querySelector('.offcanvas-body');
+const btnClearHistory = document.querySelector('.btn-clear');
 
-const input = document.querySelector(".input__numbers");
+// creating date
+const now = new Date();
+const date = Intl.DateTimeFormat(navigator.language, {
+  year: 'numeric',
+  day: 'numeric',
+  month: 'long',
+}).format(now);
 
-const equal = document.querySelector(".equal");
+class History {
+  constructor(calculation, result) {
+    this.date = date;
+    this.calculation = calculation;
+    this.result = result;
+  }
+}
 
-const btnBackSpace = document.querySelector(".btn__backspace");
-
-// Play Sound
-
-containerBtns.addEventListener("click", function (e) {
-  if (!e.target.value) return;
-
-  const btnValue = e.target.value;
-
-  //   Display number
-  input.value += btnValue;
-
-  if (btnValue === "AC") {
-    input.value = "";
+class App extends History {
+  history = [];
+  constructor() {
+    super();
+    containerBtns.addEventListener('click', this.showBtnValue.bind(this));
+    equal.addEventListener('click', this.calculateValue.bind(this));
+    btnBackSpace.addEventListener('click', this.backSpaceValue.bind(this));
+    this.getLocalStorage();
+    btnClearHistory.addEventListener('click', this.clearHistory.bind(this));
   }
 
-  if (btnValue === "back") {
-    console.log("i");
+  showBtnValue(e) {
+    if (!e.target.value) return;
+
+    const btnValue = e.target.value;
+
+    input.value += btnValue;
+
+    if (btnValue === 'AC') {
+      input.value = '';
+    }
   }
-});
 
-equal.addEventListener("click", function () {
-  if (!input.value) return;
+  calculateValue() {
+    let holdHistory = [];
 
-  const result = eval(input.value);
+    holdHistory.push(input.value);
 
-  input.value = result;
-});
+    setTimeout(() => {
+      if (!input.value) return;
 
-// Remove last character
-btnBackSpace.addEventListener("click", function () {
-  const str = input.value;
+      const result = eval(input.value);
 
-  input.value = str.slice(0, -1);
-});
+      input.value = result;
+
+      const dataHistory = new History(...holdHistory, result);
+
+      this.history.push(dataHistory);
+
+      // Set localStorage
+      this.setlocalStorage();
+
+      this.reanderHistory(dataHistory);
+    }, 100);
+  }
+
+  backSpaceValue() {
+    const str = input.value;
+    input.value = str.slice(0, -1);
+  }
+
+  reanderHistory(history) {
+    const html = `
+    <div class="d-flex justify-content-between">
+     <p>${history.calculation}</p>
+     <p>Date:${history.date}</p>
+      </div>
+     <h4> =${history.result}</h4>
+     <hr> `;
+
+    canvasContainer.insertAdjacentHTML('beforeend', html);
+  }
+
+  setlocalStorage() {
+    localStorage.setItem('history', JSON.stringify(this.history));
+  }
+
+  getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('history'));
+
+    if (!data) return;
+
+    this.history = data;
+
+    this.history.forEach((his) => this.reanderHistory(his));
+  }
+
+  clearHistory() {
+    localStorage.clear();
+    canvasContainer.innerHTML = '';
+  }
+}
+
+const app = new App();
